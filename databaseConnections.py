ae714c9988a0758d807fc0394657feb380921241
dan. teegan <dan.teegan@hotmail.com>
2020-07-09 13:47:41 +0100

    Database Connections

---
+++ databaseConnections.py
@@ -0,0 +1,10 @@
+import pyodbc
+# Establishing a database connection
+# We are trying to get the data from the database into the python console.
+
+#pyodbc is a python library tha helps you to connect to the database
+
+server = 'databases2.spartaglobal.academy'
+database = 'Northwind'
+username = 'SA'
+password = 'Passw0rd2018'
