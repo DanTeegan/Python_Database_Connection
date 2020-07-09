7f272c81a5ec625a4160fae7a5c195cc58fe5075
dan. teegan <dan.teegan@hotmail.com>
2020-07-09 16:11:17 +0100

    Database Connections

--- databaseConnections.py
+++ databaseConnections.py
@@ -1,4 +1,41 @@
 import pyodbc
+
+
+
+server = 'databases2.spartaglobal.academy'
+#databases2.spartaglobal.academy
+database = 'Northwind'
+username = 'SA'
+password = 'Passw0rd2018'
+connections = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE=' + database +';UID='+ username +';PWD='+ password)
+cursor = connections.cursor()
+print(connections)
+print(cursor)
+
+# We create a variable
+query_result = cursor.execute('SELECT * FROM Products')
+
+print("Printing query_result object:", query_result)
+
+# Code for fetching one row - use fetchone - use fetchmany if you want to fetch many rows. fetchall = fetch all rows
+rows=query_result.fetchone()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Establishing a database connection
 # We are trying to get the data from the database into the python console.
 
@@ -7,21 +44,21 @@ import pyodbc
 #pyodbc is a python library tha helps you to connect to the database
 
 # Extablishing a connection to the database that we want to use
-server = 'databases2.spartaglobal.academy'
-database = 'Northwind'
-username = 'SA'
-password = 'Passw0rd2018'
-
-
-connections = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password
-
-
-try:
-    with pyodbc.connect(connections, timeout=10) as connection:
-        print("Connection did not time out")
-except:
-    print("Connection Timed out")
-else:
-    cursor = connections.cursor()
-    print(connections)
-    print(cursor)
+# server = 'databases2.spartaglobal.academy'
+# database = 'Northwind'
+# username = 'SA'
+# password = 'Passw0rd2018'
+#
+#
+# connections = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password
+#
+#
+# # try:
+#     with pyodbc.connect(connections, timeout=10) as connection:
+#         print("Connection did not time out")
+# except:
+#     print("Connection Timed out")
+# else:
+#     cursor = connections.cursor()
+#     print(connections)
+#     print(cursor)
