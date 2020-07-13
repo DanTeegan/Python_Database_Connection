dbee71cd12588e675f1a4f589e05b2715e1f1d30
dan. teegan <dan.teegan@hotmail.com>
2020-07-13 22:22:07 +0100

    Database task

--- output.py
+++ output.py
@@ -1,7 +1,7 @@
 from querys import Queries
 # We are uding the pillar of abstraction to hide the comlexity for the user.
 
-object = Queries()  # Creating an object
+object = Queries()  # Creating the object
 
 object.query_products()
 object.query_customers_citys()
